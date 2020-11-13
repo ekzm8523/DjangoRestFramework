ff7a0bf34dbc0e193354f870fdbe2bbbc5d24157
macbook <mjw8523@gmail.com>
2020-11-13 14:37:41 +0900

    11/13

--- newsapi/news/api/views.py
+++ newsapi/news/api/views.py
@@ -44,3 +44,47 @@ def article_detail_api_view(request, pk):
     elif request.method == 'DELETE':
         article.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+from rest_framework.views import APIView
+from rest_framework.generics import get_object_or_404
+
+class ArticleListCreateAPIView(APIView):
+
+    def get(self, request):
+        articles = Article.objects.filter(active=True)
+        serializer = ArticleSerializer(articles, many=True)
+        # queryset 만 many = True , Model instance는 안해줘도 된다.
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+
+#get_object()메소드를 통해 Article 인스턴스를 리턴가능
+#self는 ArticleDetailAPIView object를 의미함
+
+class ArticleDetailAPIView(APIView):
+    def get_object(self, pk):
+        article = get_object_or_404(Article, pk=pk)
+        return article
+    
+    def get(self, request, pk):
+        article = self.get_object(pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        article = self.get_object(pk)
+        serializer = ArticleSerializer(article,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self,request,pk):
+        article = self.get_object(pk)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
